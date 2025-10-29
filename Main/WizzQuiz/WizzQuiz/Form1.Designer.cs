5fd5e9962bb08f01e93dfc4856698e23e5f41713
pardige <140949466+Pardigle@users.noreply.github.com>
2025-10-29 15:23:48 +0800

    - Added Library View.

--- Main/WizzQuiz/WizzQuiz/Form1.Designer.cs
+++ Main/WizzQuiz/WizzQuiz/Form1.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnAddQuiz = new Button();
+            btnViewAttempts = new Button();
+            lbxQuizList = new ListBox();
+            pnlLibrary = new Panel();
+            btnDeleteQuiz = new Button();
+            btnEditQuiz = new Button();
+            btnAnswerQuiz = new Button();
+            lblLibrary = new Label();
+            pnlLibrary.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnAddQuiz
+            // 
+            btnAddQuiz.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnAddQuiz.Font = new Font("Segoe UI", 10F);
+            btnAddQuiz.Location = new Point(685, 38);
+            btnAddQuiz.Name = "btnAddQuiz";
+            btnAddQuiz.Size = new Size(40, 38);
+            btnAddQuiz.TabIndex = 0;
+            btnAddQuiz.Text = "+";
+            btnAddQuiz.UseVisualStyleBackColor = true;
+            btnAddQuiz.Click += btnAddQuiz_Click;
+            // 
+            // btnViewAttempts
+            // 
+            btnViewAttempts.Font = new Font("Segoe UI", 9F);
+            btnViewAttempts.Location = new Point(501, 38);
+            btnViewAttempts.Name = "btnViewAttempts";
+            btnViewAttempts.Size = new Size(178, 38);
+            btnViewAttempts.TabIndex = 1;
+            btnViewAttempts.Text = "View Attempts";
+            btnViewAttempts.UseVisualStyleBackColor = true;
+            btnViewAttempts.Click += btnViewAttempts_Click;
+            // 
+            // lbxQuizList
+            // 
+            lbxQuizList.Font = new Font("Segoe UI", 9F);
+            lbxQuizList.FormattingEnabled = true;
+            lbxQuizList.Location = new Point(25, 81);
+            lbxQuizList.Name = "lbxQuizList";
+            lbxQuizList.ScrollAlwaysVisible = true;
+            lbxQuizList.Size = new Size(700, 264);
+            lbxQuizList.TabIndex = 2;
+            lbxQuizList.SelectedIndexChanged += lbxQuizList_SelectedIndexChanged;
+            // 
+            // pnlLibrary
+            // 
+            pnlLibrary.Controls.Add(btnAddQuiz);
+            pnlLibrary.Controls.Add(btnDeleteQuiz);
+            pnlLibrary.Controls.Add(btnEditQuiz);
+            pnlLibrary.Controls.Add(btnAnswerQuiz);
+            pnlLibrary.Controls.Add(lblLibrary);
+            pnlLibrary.Controls.Add(lbxQuizList);
+            pnlLibrary.Controls.Add(btnViewAttempts);
+            pnlLibrary.Location = new Point(24, 6);
+            pnlLibrary.Name = "pnlLibrary";
+            pnlLibrary.Size = new Size(734, 417);
+            pnlLibrary.TabIndex = 3;
+            pnlLibrary.Paint += pnlLibrary_Paint;
+            // 
+            // btnDeleteQuiz
+            // 
+            btnDeleteQuiz.Location = new Point(25, 351);
+            btnDeleteQuiz.Name = "btnDeleteQuiz";
+            btnDeleteQuiz.Size = new Size(186, 39);
+            btnDeleteQuiz.TabIndex = 6;
+            btnDeleteQuiz.Text = "Delete";
+            btnDeleteQuiz.UseVisualStyleBackColor = true;
+            btnDeleteQuiz.Click += btnDeleteQuiz_Click;
+            // 
+            // btnEditQuiz
+            // 
+            btnEditQuiz.Location = new Point(217, 351);
+            btnEditQuiz.Name = "btnEditQuiz";
+            btnEditQuiz.Size = new Size(180, 39);
+            btnEditQuiz.TabIndex = 5;
+            btnEditQuiz.Text = "Edit";
+            btnEditQuiz.UseVisualStyleBackColor = true;
+            btnEditQuiz.Click += btnEditQuiz_Click;
+            // 
+            // btnAnswerQuiz
+            // 
+            btnAnswerQuiz.Location = new Point(403, 351);
+            btnAnswerQuiz.Name = "btnAnswerQuiz";
+            btnAnswerQuiz.Size = new Size(322, 39);
+            btnAnswerQuiz.TabIndex = 4;
+            btnAnswerQuiz.Text = "Answer Quiz";
+            btnAnswerQuiz.UseVisualStyleBackColor = true;
+            btnAnswerQuiz.Click += btnAnswerQuiz_Click;
+            // 
+            // lblLibrary
+            // 
+            lblLibrary.AutoSize = true;
+            lblLibrary.Font = new Font("Segoe UI Semibold", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblLibrary.Location = new Point(9, 28);
+            lblLibrary.Name = "lblLibrary";
+            lblLibrary.Size = new Size(199, 46);
+            lblLibrary.TabIndex = 3;
+            lblLibrary.Text = "Your library";
+            lblLibrary.Click += lblLibrary_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(785, 450);
+            Controls.Add(pnlLibrary);
+            Name = "Form1";
+            Text = "WizzQuiz";
+            Load += Form1_Load;
+            pnlLibrary.ResumeLayout(false);
+            pnlLibrary.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnAddQuiz;
+        private Button btnViewAttempts;
+        private ListBox lbxQuizList;
+        private Panel pnlLibrary;
+        private Button btnAnswerQuiz;
+        private Label lblLibrary;
+        private Button btnDeleteQuiz;
+        private Button btnEditQuiz;
     }
 }
--- Main/WizzQuiz/WizzQuiz/Form1.cs
+++ Main/WizzQuiz/WizzQuiz/Form1.cs
@@ -6,5 +6,55 @@ namespace WizzQuiz
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddQuiz_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlLibrary_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lbxQuizList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnViewAttempts_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnswerQuiz_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEditQuiz_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDeleteQuiz_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblLibrary_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
