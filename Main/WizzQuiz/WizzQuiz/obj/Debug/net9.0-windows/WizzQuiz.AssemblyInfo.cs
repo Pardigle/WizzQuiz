0e3dbb6056767ebdcb6ad07739b16d78bc566399
vernal <vernalterrene@gmail.com>
2025-11-01 20:49:57 +0800

    Added functionality for btnViewAttemptFromList (viewing individual attempts))

--- Main/WizzQuiz/WizzQuiz/obj/Debug/net9.0-windows/WizzQuiz.AssemblyInfo.cs
+++ Main/WizzQuiz/WizzQuiz/obj/Debug/net9.0-windows/WizzQuiz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WizzQuiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2548d07c1d9438f7edf527c4b40d49bae672b689")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bccca32f8195a2c78dafad318871e8f55784856")]
 [assembly: System.Reflection.AssemblyProductAttribute("WizzQuiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WizzQuiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
